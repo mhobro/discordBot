462a4569c59c6c5a21f2a465e8e261d03d1c1fdb
Mark Hobro <mhobro@kth.se>
2018-01-24 23:21:04 +0100

    Inital commit

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+require('dotenv').config()
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
+
+client.login(process.env.TOKEN);
