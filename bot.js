1695e4c6db466ff8342f1957639765c6f3c8ebf0
Mark Hobro <mhobro@kth.se>
2018-01-26 21:43:00 +0100

    Fixed case sensitivity for joining channels

--- bot.js
+++ bot.js
@@ -1,14 +1,14 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const bot = new Discord.Client();
 var currChannel;
 
 require('dotenv').config()
 
-client.on("ready", () => {
+bot.on("ready", () => {
   console.log("Connected");
 });
 
-client.on("message", (message) => {
+bot.on("message", (message) => {
   if(message.content.substring(0,1) == "!"){
     var args = message.content.substring(1).split(" ");
     var action = args[0];
@@ -16,29 +16,40 @@ client.on("message", (message) => {
     if(args.length > 1){
       arg = args[1]
     }
-    switch(action){
-      case "ping":
-        message.channel.send("Hallå där, du e la go'");
-        break;
-      case "join":
-        channel = client.channels.find('name', arg);
-        currChannel = channel;
+    handleAction(arg, action);
+  }
+});
+
+function handleAction(arg, action){
+  switch(action){
+    case "ping":
+      message.channel.send("Hallå där, du e la go'");
+      break;
+    case "join":
+      currChannel = getChannel(arg);
+      if(channel){
         channel.join().then(connection =>
           console.log('Connected to '+ arg)
         ).catch(console.error);
         break;
-      case "leave":
-        if(currChannel){
-            currChannel.leave();
-            currChannel = NULL;
-        }
-        break;
-      default:
-        break;
-    }
+      }
+    case "leave":
+      if(currChannel){
+          currChannel.leave();
+      }
+      break;
+    default:
+      break;
   }
-});
+}
+
+function getChannel(name) {
+    for (var channel of bot.channels) {
+        if (channel[1].name.toLowerCase() === name.toLowerCase()) {
+            return channel[1];
+        }
+    }
+}
 
 
-
-client.login(process.env.TOKEN);
+bot.login(process.env.TOKEN);
